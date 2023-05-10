bc6119ead638876202b216e459e969e5e4450a73
linyu0801 <spe9g0908@gmail.com>
2023-05-10 23:19:24 +0800

    google search console

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,18 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { AuthProvider } from "../hooks/useAuth";
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from "recoil";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { AuthProvider } from '../hooks/useAuth';
+import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="gqKw61TEp6ewJ3y8J5W6n6a_qSlFDX5CH2iMZ8X4l1s"
+        />
+      </Head>
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
